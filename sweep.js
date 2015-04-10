0833a276d703a55b926d0e87a3b9e08c0464557d
Don Park <donp@donp.org>
2015-04-10 09:06:48 -0700

    use Decimal.js. even subtraction can introduce a rounding error

--- sweep.js
+++ sweep.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var Decimal = require('decimal.js')
 var Chain = require('chain-node')
 var btcjs = require('bitcoinjs-lib')
 
@@ -8,23 +9,27 @@ var one_satoshi_in_btc = 0.00000001
 
 var from = btcjs.ECKey.fromWIF(process.argv[2])
 var to = process.argv[3]
-var amount = 0
+var to_amount = process.argv[4]
+var balance = new Decimal(0)
 var from_addr = from.pub.getAddress().toString()
 chain.getAddressUnspents(from_addr, function(err, unspents) {
   var tx = new btcjs.TransactionBuilder()
   unspents.forEach(function(out){
     console.log(from_addr, 'unspent', out.value * one_satoshi_in_btc)
-    amount = amount + out.value
+    balance.plus(out.value)
     tx.addInput(out.transaction_hash, out.output_index)
   })
-  if (amount > 0) {
-    console.log('sending', amount * one_satoshi_in_btc, 'to', to)
-    tx.addOutput(to, amount)
+  console.log('from', from_addr, 'balance', balance * one_satoshi_in_btc)
+  if (balance > 0) {
+    var change = balance.minus(to_amount)
+    console.log('sending', to_amount * one_satoshi_in_btc, 'to', to, 'change', change)
+    tx.addOutput(to, to_amount)
+    tx.addOutput(from, parseInt(change))
     tx.sign(0,from)
     var payload = tx.build().toHex()
-    chain.sendTransaction(payload, function(err, resp) {
-      console.log('tx sumitted!', err, resp)
-    })
+//    chain.sendTransaction(payload, function(err, resp) {
+//      console.log('tx sumitted!', err, resp)
+//    })
   } else {
     console.log('from address', from_addr, 'is empty.')
   }
