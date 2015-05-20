b0d50337484a3e4ac013cb8fb21ce980a39301e7
Don Park <donp@donp.org>
2015-05-19 20:43:36 -0700

    more help

--- copper.js
+++ copper.js
@@ -68,31 +68,61 @@ if(cmd === 'new') {
 }
 
 if(cmd === 'tx') {
-  var from = args._.shift()
-  var walletKey = copper.keyFind(keys, args.in)
-  if(walletKey) {
-    var inKey = copper.fromWIF(walletKey.priv)
-    copper.unspents(walletKey).then(function(unspents){
-      tx = copper.transaction()
-      unspents.map(function(u){
-        var inHash = unspents[0].tx_hash
-        tx.addInput(inHash, 0)
-        console.log(' input:', walletKey.pub, u.value/100000000+"btc")
+  if(args.in) {
+    var walletKey = copper.keyFind(keys, args.in)
+    if(walletKey) {
+      var inKey = copper.fromWIF(walletKey.priv)
+      copper.unspents(walletKey).then(function(unspents){
+        tx = copper.transaction()
+        var available = 0
+        unspents.map(function(u){
+          var inHash = unspents[0].tx_hash
+          available = available + u.value
+          tx.addInput(inHash, 0)
+          console.log(' input:', walletKey.pub, u.value/100000000+"btc")
+        })
+        if(args.out) {
+          if(args.amount) {
+            var satoshi_send = args.amount*100000000
+            if(available >= satoshi_send) {
+              tx.addOutput(args.out, satoshi_send)
+              console.log('output:', args.out, args.amount+"btc")
+              tx.sign(0, inKey)
+              console.log('TX:')
+              console.log(tx.build().toHex())
+            } else {
+              console.log('input balance of', available/100000000+"btc",
+                          'is insufficient for', args.amount)
+            }
+          } else {
+            console.log('missing: amount to send with --amount <amount of btc>')
+          }
+        } else {
+          console.log('missing: where to send with --out <address>')
+        }
       })
-      tx.addOutput(args.out, args.amount*100000000)
-      console.log('output:', args.out, args.amount+"btc")
-      tx.sign(0, inKey)
-      console.log('TX:')
-      console.log(tx.build().toHex())
-    })
+    } else {
+      console.log('no private key found for', args.in)
+      if(args.wallet) {
+        console.log('in wallet', args.wallet)
+      } else {
+        console.log('missing: wallet file --wallet <filename>')
+      }
+    }
   } else {
-    console.log('no private key found for', args.in)
+    console.log('missing: send from --in <address>')
   }
 }
 
 function help() {
+  console.log('dust v0.01')
   console.log('usage: dust <command> [<args>]')
   console.log('$ dust new')
-  console.log('$ dust balance <bitcoin address>')
-  console.log('$ dust tx --in <address> --out <address> --amount <btc>')
+  console.log('creates new bitcoint address and private key')
+  console.log('$ dust balance <bitcoin address>|--wallet <filename>')
+  console.log('check the balance of an address or an entire wallet')
+  console.log('$ dust tx --in <address> --out <address> --amount <btc> --wallet <filename>')
+  console.log('builds bitcoin transaction. Safe to use - DOES NOT SEND.')
+  console.log('Use https://blockchain.info/pushtx to submit transaction.')
+  console.log('supported wallet format: dump file from bitcoin-core')
 }
