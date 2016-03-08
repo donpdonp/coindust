ad11e9f6b5e7676973509bc1777ebdd1982e1fa4
Don Park <donp@donp.org>
2016-03-08 11:53:27 -0800

    standard-format run

--- test/index.js
+++ test/index.js
@@ -4,12 +4,11 @@ var fs = require('fs')
 var should = require('chai').should()
 var coindust = require('../lib/coindust')
 
-describe('cli generate config', function() {
-  it('creates a blank config and wallet', function() {
-
-    var empty_config_dir = "./test/configs/empty"
-    var config_file = empty_config_dir+"/config.json"
-    var wallet_file = empty_config_dir+"/wallet.json"
+describe('cli generate config', function () {
+  it('creates a blank config and wallet', function () {
+    var empty_config_dir = './test/configs/empty'
+    var config_file = empty_config_dir + '/config.json'
+    var wallet_file = empty_config_dir + '/wallet.json'
 
     // prepare empty config dir
     try {
