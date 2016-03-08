277f2c2f9ed5c5b66ad143bebf3f44b9095ae066
Don Park <donp@donp.org>
2016-03-08 11:29:02 -0800

    cli, config_dir rejiggering for tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var should = require('chai').should()
+var coindust = require('../lib/coindust')
 
 describe('#versionGuard', function() {
   it('cli', function() {
+    coindust("./test/configs/empty")
     should.equal(true, true);
   })
 
